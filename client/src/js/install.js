02123fc19a126d1dad784a024da2c4c9a15c3f8f
beattycharles <113803621+beattycharles@users.noreply.github.com>
2023-03-06 22:12:31 -0500

    Starter code and boiler plates added

---
+++ client/src/js/install.js
@@ -0,0 +1,21 @@
+const butInstall = document.getElementById("buttonInstall");
+
+// Event handler for prompt before you install the app
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+});
+
+// Event handler for when you click the install button
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+});
+
+// Event handler for when the app is installed
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
---
+++ client/webpack.config.js
@@ -0,0 +1,81 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = () => {
+  return {
+    mode: "development",
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    // This updates the page without a full page refresh
+    devServer: {
+      hot: "only",
+    },
+    plugins: [
+      // Automaticallty makes a copy of the index.html in root directory and puts it in the dist folder
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      // Makes a copy of manifest from root direcctory and places it in the dist directory
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // Creates service worker
+      new WorkboxPlugin.GenerateSW(),
+      // Specifies the metadata properties used in the app defined in the manifest
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Takes notes with JavaScript syntax highlighing!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("src", "icons"),
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          // Adds css loader
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          // Adds babel loader
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
